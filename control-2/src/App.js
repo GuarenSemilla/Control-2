a7d2a0ee5a8b01fa7c5b176e8093ce21787e315d
Nicolas Verdugo <164950770+GuarenSemilla@users.noreply.github.com>
2024-06-03 15:53:42 -0400

    pauta control

---
+++ control-2/src/App.js
@@ -0,0 +1,20 @@
+import { Container, Row, Col} from 'react-bootstrap';
+
+function AutoLayoutSizingExample() {
+  return (
+    <Container>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col xs={6}>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col xs={5}>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default AutoLayoutSizingExample;
